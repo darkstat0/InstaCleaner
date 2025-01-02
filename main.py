317f46e3c1bc27abb3deab6a1d170eb2ea160d40
kenjahacker <kenjaev.hasan001@gmail.com>
2025-01-03 00:59:51 +0500

    darkstar

---
+++ main.py
@@ -0,0 +1,24 @@
+from instagrapi import Client
+import time
+
+# Авторизация
+username = "khasank55"
+password = "Kenjaev00177266255"
+cl = Client()
+cl.login(username, password)
+
+# Получение подписчиков и подписок
+followers = cl.user_followers(cl.user_id)
+following = cl.user_following(cl.user_id)
+
+for user_id, user_info in following.items():
+    if user_id in followers:
+        print(f"Пропускаем: {user_info.username} (Подписан на вас)")
+    else:
+        print(f"Отписка от: {user_info.username}")
+        try:
+            cl.user_unfollow(user_id)
+            print(f"Успешно отписались от {user_info.username}")
+        except Exception as e:
+            print(f"Ошибка при обработке {user_info.username}: {e}")
+        time.sleep(5)  # Добавляем паузу между запросами
